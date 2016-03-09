a5eaf0d59e3ef00f6c12694a88a25c7d3564f292
justin8888 <jusitnmd@126.com>
2016-03-09 15:56:30 -0800

    BW_JAVA_OPTS

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -51,6 +51,53 @@ setLogLevel()
 	fi
 }
 
+
+checkEnvSubstituteConfig()
+{
+	bwappnodeTRA=$HOME/tibco.home/bw*/*/bin/bwappnode.tra
+	appnodeConfigFile=$HOME/tibco.home/bw*/*/config/appnode_config.ini
+if [[ ${BW_JAVA_OPTS} ]]; then
+		if [ -e ${bwappnodeTRA} ]; then
+			 sed -i.bak "/java.extended.properties/s/$/ $BW_JAVA_OPTS/" $bwappnodeTRA
+			 echo "appended $BW_JAVA_OPTS to java.extend.properties"
+		fi
+fi
+
+if [[ ${BW_ENGINE_THREADCOUNT} ]]; then
+		if [ -e ${appnodeConfigFile} ]; then
+			printf '%s\n' "bw.engine.threadCount=$BW_ENGINE_THREADCOUNT" >> $appnodeConfigFile
+			echo "set BW_ENGINE_THREADCOUNT to $BW_ENGINE_THREADCOUNT"
+		fi
+fi
+if [[ ${BW_ENGINE_STEPCOUNT} ]]; then
+		if [ -e ${appnodeConfigFile} ]; then
+			printf '%s\n' "bw.engine.stepCount=$BW_ENGINE_STEPCOUNT" >> $appnodeConfigFile
+			echo "set BW_ENGINE_STEPCOUNT to $BW_ENGINE_STEPCOUNT"
+		fi
+fi
+if [[ ${BW_APPLICATION_JOB_FLOWLIMIT} ]]; then
+		if [ -e ${appnodeConfigFile} ]; then
+
+			printf '%s\n' "bw.application.job.flowlimit.$bwBundleAppName=$BW_APPLICATION_JOB_FLOWLIMIT" >> $appnodeConfigFile
+			echo "set BW_APPLICATION_JOB_FLOWLIMIT to $BW_APPLICATION_JOB_FLOWLIMIT"
+		fi
+fi
+
+if [[  $BW_LOGLEVEL = "DEBUG" ]]; then
+	if [[ ${BW_APPLICATION_JOB_FLOWLIMIT} ]] || [[ ${BW_ENGINE_STEPCOUNT} ]] || [[ ${BW_ENGINE_THREADCOUNT} ]]; then
+		echo "---------------------------------------"
+		cat $appnodeConfigFile
+		echo "---------------------------------------"
+	fi
+fi
+}
+
+
+
+
+
+
+
 export BW_KEYSTORE_DIR=/resources/addons/certs
 if [ ! -d $HOME/tibco.home ];
 then
@@ -71,6 +118,7 @@ then
 	sed -i.bak "s#_APPDIR_#$HOME#g" $HOME/tibco.home/bw*/*/config/appnode_config.ini
 	unzip -qq `echo $HOME/tibco.home/bw*/*/bin/bwapp.ear` -d /tmp
 	setLogLevel
+	checkEnvSubstituteConfig
 	cd /java-code
 	$HOME/tibco.home/tibcojre64/1.*/bin/javac -cp `echo $HOME/tibco.home/bw*/*/system/shared/com.tibco.tpcl.com.fasterxml.jackson_*`/*:.:$HOME/tibco.home/tibcojre64/1.*/lib ProfileTokenResolver.java
 fi
