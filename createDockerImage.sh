a38578c26b82a37e1daa3eb4b8f207438c51ff4c
wgu <wgu@tibco.com>
2016-04-28 13:54:28 -0700

    createDockerImage.sh

---
+++ createDockerImage.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Copyright (c) 2016, TIBCO Software Inc. All rights reserved.
+# You may not use this file except in compliance with the license 
+# terms contained in the TIBCO License.md file provided with this file.
+
+
+if [[ $# -lt 1 || $# -gt 2 ]]; then
+    echo "Usage: ./createDockerImage.sh <path/to/bwce_cf.zip> <Tag>"
+    printf "\t %s \t\t %s \n\t\t\t\t %s \n" "Location of runtime zip(bwce_cf.zip)"
+    printf "\t %s \t\t %s \n\t\t\t\t %s \n" "Tag. Eg: bwce:v2.0.0"
+    exit 1
+fi
+zipLocation=$1
+
+if [ -z "$2"  ]; then
+	tag="bwce:v2.0.0"
+	echo "Tag is set to bwce:v2.0.0"
+else
+	tag=$2
+fi
+
+mkdir -p resources/bwce-runtime && cp -i $zipLocation "$_"
+
+docker build -f Dockerfile -t $tag .
