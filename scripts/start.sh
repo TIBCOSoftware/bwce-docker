3dfac994bc0c6698c31e53490049e5e7d6ba688c
Wanghai Gu <wgu@tibco.com>
2016-10-19 14:14:44 -0700

    Update start.sh
    https://jira.tibco.com/browse/BWCE-763

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,7 +6,7 @@ export MALLOC_MMAP_THRESHOLD_=1024
 export MALLOC_TRIM_THRESHOLD_=1024
 export MALLOC_MMAP_MAX_=65536
 export BW_KEYSTORE_PATH=/resources/addons/certs
-bash /scripts/setup.sh
+. ./scripts/setup.sh
 STATUS=$?
 if [ $STATUS == "1" ]; then
     echo "ERROR: Failed to setup BWCE runtime. See logs for more details."
