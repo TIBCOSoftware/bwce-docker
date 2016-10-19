0236cc5ad8703e6fbf0e60cb6f3518effe9704e1
wgu <wgu@tibco.com>
2016-10-19 14:14:00 -0700

    https://jira.tibco.com/browse/BWCE-763

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,11 +6,10 @@ export MALLOC_MMAP_THRESHOLD_=1024
 export MALLOC_TRIM_THRESHOLD_=1024
 export MALLOC_MMAP_MAX_=65536
 export BW_KEYSTORE_PATH=/resources/addons/certs
-bash /scripts/setup.sh
+. ./scripts/setup.sh
 STATUS=$?
 if [ $STATUS == "1" ]; then
     echo "ERROR: Failed to setup BWCE runtime. See logs for more details."
     exit 1
 fi
-export JAVA_HOME=$HOME/tibco.home/tibcojre64/1.8.0
 exec bash $HOME/tibco.home/bw*/*/bin/startBWAppNode.sh
