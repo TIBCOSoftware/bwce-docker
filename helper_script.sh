addec14f7be15dc910b072b7953a25003fb06bae
Nitish Goel <goelnitish@gmail.com>
2018-09-25 18:38:30 +0000

    helper script mods

--- helper_script.sh
+++ helper_script.sh
@@ -1,6 +1,9 @@
-#Command to Execute this script - 
-#sh helper_script.sh ~/Desktop/nitish-files-backup/2.4.0/bwce-runtime-2.4.0.zip rest
-
+#this helper script --
+	#creates the base docker image
+	#creates the application image for a particular sample
+	#runs the sample
+	#Command to Execute this script - 
+	#sh helper_script.sh ~/Desktop/nitish-files-backup/2.4.0/bwce-runtime-2.4.0.zip <optional - sample app name> <optional - sample app arguments>
 
 #Docker clean-up commands
 #=========================
@@ -12,13 +15,13 @@ docker stop $(docker ps -q -a)
 #=========================
 
 if [[ $# -lt 1 ]]; then
-    echo "Usage: ./helper_script.sh <path/to/bwce-runtime-2.4.0.zip>"
+    echo "Usage: ./helper_script.sh <path/to/bwce-runtime-2.4.0.zip> <optional - sample_application_name> <optional - sample application arguments>"
     printf "\t %s \t\t %s \n\t\t\t\t %s \n" "Location of runtime zip (bwce-runtime-<version>.zip)"
+	printf "\t %s \t\t %s \n\t\t\t\t %s \n" "Optional Argument - Sample application to run; for example - rest, http, jms"
     exit 1
 fi
 zipLocation=$1
 
-cd c:/bwce/bwce-docker
 #./createDockerImage.sh ~/Desktop/nitish-files-backup/2.4.0/bwce-runtime-2.4.0.zip
 ./createDockerImage.sh $zipLocation
 
@@ -33,6 +36,8 @@ sampleApplication=$2
 
 #HTTP Sample Commands
 #=========================
+
+#Example Command: sh helper_script.sh ~/Desktop/nitish-files-backup/2.4.0/bwce-runtime-2.4.0.zip http
 if [ "$sampleApplication" = "http" ]; then
 	cd examples/HTTP
 	docker build -t bwce-http-app-nano .
@@ -41,22 +46,31 @@ fi
 
 #REST Sample Commands
 #=========================
+
+#Example Command: sh helper_script.sh ~/Desktop/nitish-files-backup/2.4.0/bwce-runtime-2.4.0.zip rest
 if [ "$sampleApplication" = "rest" ]; then
 	cd examples/REST
 	docker build -t bwce-rest-app-nano .
-	docker run -it -P -e BW_LOGLEVEL=DEBUG bwce-rest-app-nano
+	docker run -it -P -e BW_LOGLEVEL=error bwce-rest-app-nano
 fi
 
 #JMS Sample Commands
 #=========================
+
+#Example Command: sh helper_script.sh ~/Desktop/nitish-files-backup/2.4.0/bwce-runtime-2.4.0.zip jms tcp://13.56.67.132:7222
 if [ "$sampleApplication" = "jms" ]; then
+	url=$3
+	echo $url
 	cd examples/JMS
 	docker build -t bwce-jms-app-nano .
-	docker run -ti -e EMS_URL="tcp://13.56.67.132:7222" -e EMS_QUEUE="jmsbasic.queue" -e REPLY_QUEUE="reply.queue" -e BW_PROFILE="docker" bwce-jms-app-nano
+	#docker run -ti -e EMS_URL="tcp://13.56.67.132:7222" -e EMS_QUEUE="jmsbasic.queue" -e REPLY_QUEUE="reply.queue" -e BW_PROFILE="docker" bwce-jms-app-nano
+	docker run -ti -e EMS_URL=$url -e EMS_QUEUE="jmsbasic.queue" -e REPLY_QUEUE="reply.queue" -e BW_PROFILE="docker" bwce-jms-app-nano
 fi
 
 #Hystrix Sample Commands
 #=========================
+
+#Example Command: sh helper_script.sh ~/Desktop/nitish-files-backup/2.4.0/bwce-runtime-2.4.0.zip hystrix
 if [ "$sampleApplication" = "hystrix" ]; then
 	cd examples/Hystrix
 	docker build -t bwce-hystrix-app-nano .
@@ -65,23 +79,34 @@ fi
 
 #JMS with Consul Sample Commands
 #=========================
+
+#Example Command: sh helper_script.sh ~/Desktop/nitish-files-backup/2.4.0/bwce-runtime-2.4.0.zip consul-jms http://13.57.245.44:8500/
 if [ "$sampleApplication" = "consul-jms" ]; then
+	url=$3
 	cd examples/consul/client
 	docker build -t bwce-consul-sd-client-nano .
 	cd c:/bwce/bwce-docker/examples/consul/server
 	docker build -t bwce-consul-sd-server-nano .
-
-	docker run -d -e CONSUL_SERVER_URL=http://13.57.245.44:8500/ -p 18087:8080 -e SERVICE_NAME=BWCE-HELLOWORLD-SERVICE bwce-consul-sd-server-nano:latest
-	docker run -d -e CONSUL_SERVER_URL=http://13.57.245.44:8500/ -p 18086:8080 -e SERVICE_NAME=BWCE-HELLOWORLD-SERVICE bwce-consul-sd-client-nano:latest
+	#docker run -d -e CONSUL_SERVER_URL=http://13.57.245.44:8500/ -p 18087:8080 -e SERVICE_NAME=BWCE-HELLOWORLD-SERVICE bwce-consul-sd-server-nano:latest
+	#docker run -d -e CONSUL_SERVER_URL=http://13.57.245.44:8500/ -p 18086:8080 -e SERVICE_NAME=BWCE-HELLOWORLD-SERVICE bwce-consul-sd-client-nano:latest
+	docker run -d -e CONSUL_SERVER_URL=$url -p 18087:8080 -e SERVICE_NAME=BWCE-HELLOWORLD-SERVICE bwce-consul-sd-server-nano:latest
+	docker run -d -e CONSUL_SERVER_URL=$url -p 18086:8080 -e SERVICE_NAME=BWCE-HELLOWORLD-SERVICE bwce-consul-sd-client-nano:latest
 fi
 
 
 #RestBookstore(JDBC) Sample Commands
 #=========================
+
+#Example Command: sh helper_script.sh ~/Desktop/nitish-files-backup/2.4.0/bwce-runtime-2.4.0.zip jdbc http://13.57.245.44:8500/ nitish123 nitish456
 if [ "$sampleApplication" = "jdbc" ]; then
+	
+	db_url=$3
+	db_username=$4
+	db_password=$5
+	
 	cd examples/JDBC
 	docker build -t bwce-jdbc-app-nano .
 	#docker run -p 8080:8080 -e BW_LOGLEVEL=DEBUG bwce-jdbc-app-nano [use this if vales are hardcoded in the applicatiopn/module properties]
-	docker run -p 8080:8080 -e BW_LOGLEVEL=DEBUG -e DB_URL="xxx-xxx-xxx" -e DB_USERNAME="dummy" -e DB_PASSWORD="nitish" bwce-jdbc-app-nano
+	docker run -p 8080:8080 -e BW_LOGLEVEL=DEBUG -e DB_URL=$db_url -e DB_USERNAME=$db_username -e DB_PASSWORD=$db_password bwce-jdbc-app-nano
 fi
 
